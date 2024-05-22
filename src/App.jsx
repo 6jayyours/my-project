75a8870b8680f021f8cbb17b60e8bfddc2e0c690
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-05-22 10:33:56 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
   return (
-      <div>
-        <h1>ARJUN</h1>
-      </div>
+      <>
+      <Navbar/>
+      </>
   )
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      hai
+    </div>
+  )
+}
+
+export default Navbar
